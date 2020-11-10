29cbf1db0e8d323c4c81721406889dc79b8528b2
soleilyasmina <mounlasolomon@gmail.com>
2020-11-10 10:36:33 -0500

    update to react 17, and add valid href

--- src/Timeline.js
+++ src/Timeline.js
@@ -8,7 +8,7 @@ function Timeline() {
         <div className="user-info">
           <img src="https://ga-core.s3.amazonaws.com/cms/files/files/000/000/886/original/ga-logo-gear.png" alt="General Assembly Logo" className="profile-pic" />
           <div className="user-text">
-            <p><a href="">General Assembly</a></p>
+            <p><a href="https://generalassemb.ly/">General Assembly</a></p>
             <p><small>April 3 at 10:25 AM</small></p>
           </div>
         </div>
